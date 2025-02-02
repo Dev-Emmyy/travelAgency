5168655818e66001f5beab282a104ce227ba6df8
Emmanuel <115547557+Dev-Emmyy@users.noreply.github.com>
2025-02-02 01:57:48 +0100

    commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,26 +5,18 @@ export const metadata = {
   title: "Travel Agency",
   description: "Travel around the world easily",
   icons: {
-    icon: [
-      {
-        url: '/appLogo.png',
-        href: '/appLogo.png',
-      }
-    ],
-    shortcut: '/appLogo.png',
-    apple: '/appLogo.png',
+    icon: "/appLogo.png", // Path to your favicon
+    shortcut: "/appLogo.png", // Shortcut icon for browsers
+    apple: "/appLogo.png", // Apple touch icon
   },
-  manifest: '/manifest.json'
+  manifest: "/manifest.json", // Path to your manifest file
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SessionProviderWrapper>
-          {children}
-        </SessionProviderWrapper>
+        <SessionProviderWrapper>{children}</SessionProviderWrapper>
       </body>
     </html>
   );

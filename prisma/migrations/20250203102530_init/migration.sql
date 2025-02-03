db6f13b6903800cb6f835c840999064d22fc8173
Emmanuel <115547557+Dev-Emmyy@users.noreply.github.com>
2025-02-03 11:29:32 +0100

    Configured production database

---
+++ prisma/migrations/20250203102530_init/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
+  - The `id` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `password` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "emailVerified",
+ALTER COLUMN "email" SET NOT NULL,
+ALTER COLUMN "password" SET NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
